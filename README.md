dee3e2b0dcff1475c2dd84b61f41c04c51206c40
nduran2 <68115066+nduran2@users.noreply.github.com>
2021-03-30 13:51:25 +0000

    Inicial

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
-# cf_lpd_plugin_template
-Template de plugin launchpad en Cloud Foundry
+Configurar parámetros en manifest.json, crossNavigation. Verificar que la url coincida con la del snippet que proporciona SWA.
 
-Agregar el código en Component.js
+
+    "parameters": { 
+        "SWA_PUB_TOKEN": { "defaultValue": { "value": "1a433f85-f5ff-4c6d-98e2-b128164d43b5" } },
+        "SWA_BASE_URL": { "defaultValue": { "value": "https://events.wa.cfapps.eu10.hana.ondemand.com/tracker/" } }
+    },
---
+++ pluginswa/webapp/Component.js
@@ -0,0 +1,92 @@
+sap.ui.define([
+    "sap/ui/core/UIComponent",
+    "sap/ui/Device",
+    "hb4/pluginswa/model/models"
+], function (UIComponent, Device, models) {
+    "use strict";
+
+
+
+    return UIComponent.extend("hb4.pluginswa.Component", {
+
+        metadata: {
+            manifest: "json"
+        },
+
+        init: function () {
+            var oPluginParameters = this.getComponentData().config; // obtain plugin parameters
+            var SWA_PUB_TOKEN = "", 
+                TRACKING_URL = "", 
+                USR = "";
+            //Read the variables and pass them
+            SWA_PUB_TOKEN = oPluginParameters.SWA_PUB_TOKEN;
+            TRACKING_URL = oPluginParameters.SWA_BASE_URL;
+            //USR = oPluginParameters.SWA_OWNER;
+            USR = sap.ushell.Container.getUser().getEmail();
+            this.loadSWAObject(SWA_PUB_TOKEN, TRACKING_URL, true, USR);
+            this.loadWebGUI();
+        },
+
+        // create SWA object and load track.js
+        loadSWAObject: function (SWA_PUB_TOKEN, TRACKING_URL, LOGGING, usr) {
+            window.swa = {
+                pubToken: SWA_PUB_TOKEN,
+                baseUrl: TRACKING_URL,
+                loggingEnabled: LOGGING,
+                subSiteId: this.setSubSiteId,
+                owner: usr
+            };
+
+            var d = document, g = d.createElement("script"), s = d.getElementsByTagName("script")[0];
+            g.type = "text/javascript";
+            g.defer = true;
+            g.async = true;
+            g.src = window.swa.baseUrl + "js/track.js";
+            s.parentNode.insertBefore(g, s);
+            if ((typeof sap !== "undefined" && typeof sap.ui !== "undefined") && sap.ui.version < "1.65.0") {
+                window.onhashchange = function () {
+                    window.swa.trackLoad();
+                };
+            }
+        },
+
+        /*Method which listens to web gui announce event and then send the postmessage with baseurl,pubtoken and type as request*/
+        loadWebGUI: function () {
+            if (window.addEventListener) {
+                window.addEventListener("message", function (e) {
+                    var guiresponse = JSON.parse(e.data);
+                    if (guiresponse.service === "sap.its.readyToListen" && guiresponse.type === "announce") {
+                        var swaCookies = escape(window.parent.document.cookie);
+                        var body = {
+                            "type": "request",
+                            "service": "sap.its.trackSWA",
+                            "pubToken": window.swa.pubToken,
+                            "baseUrl": window.swa.baseUrl,
+                            "message": "Response from SWA",
+                            "isConsentGiven": true,
+                            "cookieVal": swaCookies
+                        };
+                        //post the message with pub token and base url
+                        e.source.postMessage(JSON.stringify(body), e.origin);
+                    }
+                });
+            }
+        },
+
+
+
+        //Set the subsiteid as each FLP tile where a system acts as a site
+        setSubSiteId: function () {
+            var subSite = "";
+
+            if (window.location.href.substring(window.location.href.indexOf("#")) !== -1) {
+                subSite = window.location.href.substring(window.location.href.indexOf("#") + 1);
+            }
+            if (subSite.indexOf("&") !== -1) {
+                subSite = subSite.substring(0, subSite.indexOf("&"));
+            }
+
+            return (subSite !== "") ? subSite : undefined;
+        }
+    });
+});
